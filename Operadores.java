08f05253f996b7ce855d72f5512755aa97677331
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-06-03 22:45:53 -0600

    Create Operadores.java

---
+++ Operadores.java
@@ -0,0 +1,21 @@
+public class Operadores2 {
+    public static void main(String[] args) {
+        System.out.print(" ****** AND ******\n");
+        System.out.print(" true && true : \n");
+        System.out.print(" true && false : %d\n");
+        System.out.print(" false && true : \n");
+        System.out.print(" false && false : \n");
+    
+        System.out.print(" ****** OR ******\n");
+        System.out.print(" true || true : \n");
+        System.out.print(" true || false : \n");
+        System.out.print(" false || true : \n");
+        System.out.print(" false || false : \n");
+    
+        System.out.print(" ****** XOR ******\n");
+        System.out.print(" true ^ true : \n");
+        System.out.print(" true ^ false : \n");
+        System.out.print(" false ^ true : \n");
+        System.out.print(" false ^ false : \n");
+    }    
+}
